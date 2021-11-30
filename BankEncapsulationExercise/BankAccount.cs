bc484ddc6e14e09ae23e76b7963da262fa136a90
Thad <thadisfree@gmail.com>
2021-11-30 10:05:13 -0600

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance;
+
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount,0:c} to your account.");
+            _balance = amount;      
+        }
+
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+
+      
+        
+        //public double newbalance;
+
+        //public BankAccount(double amount)
+        //{
+        //    balance = amount;
+        //}
+        
+
+        //public void MakeDeposit(double depositAmount)
+        //{
+        //    balance += depositAmount;
+            
+        //}
+        //public void MakeWithdrawal (double withdrawalAmount)
+        //{
+        //    balance -= withdrawalAmount;
+        //}
+        //public double GetBalance()
+        //{
+        //    balance = newbalance;
+        //        return newbalance;
+        //}
+
+        
+
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much $$$ do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double newbalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current Balance is {newbalance, 0:c}");
+
+
+
+        }
+    }
+}
